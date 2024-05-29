5188eb2e24d0db9dc4ca5b6be6b91df14a25bbfb
David Wenk <davidwenk@icloud.com>
2024-05-29 14:50:16 +0200

    add module validations

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,14 @@
 class Costume < ApplicationRecord
+  CATEGORIES = ["superhero", "historical", "movie", "tv show", "animal", "other"]
+  SIZES = ["S", "M", "L", "XL", "XXL", "other"]
+
+  # validations 
+  validates :name, :category, :size, :description, :price_per_day, presence: true
+  validates :name,  :description, uniqueness: true
+  validates :name, :description, length: { minimum: 5 }
+  validates :price_per_day, numericality: { greater_than: 0 }
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :size, inclusion: { in: SIZES }
+
+  # associations 
 end
