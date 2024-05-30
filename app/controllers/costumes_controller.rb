ad4799a26f55002ab27d8b5e203e2e8ae9a5d6da
Robert Baufeld <rbaufeld@posteo.de>
2024-05-30 11:42:55 +0200

    added new and create action

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -9,8 +9,21 @@ class CostumesController < ApplicationController
   end
 
   def new
+    @costume = Costume.new
   end
 
   def create
+    @costume = Costume.new(costume_params)
+    if @costume.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def costume_params
+    params.require(:costume).permit(:name, :description, :size, :price_per_day, photos: [])
   end
 end
