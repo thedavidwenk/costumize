b27ffa414fba5c679f68a7c659e44652d58961fd
Robert Baufeld <rbaufeld@posteo.de>
2024-05-30 11:25:49 +0200

    pause

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,8 +1,11 @@
 class CostumesController < ApplicationController
+  before_action only: [:show, :new, :create]
+
   def home
   end
 
   def index
+    @costumes = Costume.all
   end
 
   def show
@@ -12,5 +15,15 @@ class CostumesController < ApplicationController
   end
 
   def create
+  end
+
+  private
+
+  def set_costumes
+    @costume = Costume.find(params[:id])
+  end
+
+  def costume_params
+    params.require(:costume).permit(:name, :category, :size, :description, :price_per_day, photos: [])
   end
 end
