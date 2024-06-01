e42723071607d4fcac191034ed2b9bca3f954557
Gabriela Cardoso <157655310+gabicards@users.noreply.github.com>
2024-06-01 11:27:32 +0200

    Added code explanations to the search_controller.js file

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
 
   connect() {
     console.log("SearchController connected");
-    const form = this.element.querySelector('form');
+    const form = this.element.querySelector('form'); // This variable is  used throughout the code to refer to the search form
     if (form) {
       form.addEventListener('input', this.search.bind(this));
     } else {
@@ -30,7 +30,8 @@ export default class extends Controller {
       return;
     }
 
-    const url = form.action;
+    const url = form.action; // the "form" variable was set  on line #21, form.action retrieves the URL to which the form is set to submit its data
+    // "input->search#search" defines the form action in our _search.html.erb file
     const params = new URLSearchParams(new FormData(form)).toString();
     console.log(`Fetching results from: ${url}?${params}`);
 
@@ -43,22 +44,29 @@ export default class extends Controller {
       .then(response => response.text())
       .then(html => {
         console.log("Received HTML response");
-        this.resultsTarget.innerHTML = html;
+        this.resultsTarget.innerHTML = html; // This ensures that only the "data-search-target="results" div in the index.html.erb file changes
       })
       .catch(error => {
         console.error("Error:", error);
       });
   }
 
-  allFieldsEmpty(form) {
-    const formData = new FormData(form);
-    for (const value of formData.values()) {
-      if (value.trim() !== "") {
+  allFieldsEmpty(form) { // Checking if all fields in a form are empty
+    const formData = new FormData(form); // Creates a FormData object from the given "form" element
+    for (const value of formData.values()) { // This is a loop that iterates over each value in the FormData object and yields the values of the form fields
+      if (value.trim() !== "") { // This removes any whitespace from both ends of the string and checks whether it is empty
         return false;
       }
     }
     return true;
   }
+
+  // Referring to formData.values(), these are the values in formData:
+
+  // name field: " " (string with spaces)
+  // category field: "" (empty string)
+  // size field: "" (empty string)
+  // available_date field: "" (empty string)
 
   loadAllResults(form) {
     const url = form.action;
