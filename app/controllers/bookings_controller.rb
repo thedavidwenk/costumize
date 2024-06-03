6bb72d50aad07fcb60456bf7c20584cdc99fc1b4
carmen lopez <clarroquia@gmail.com>
2024-06-03 12:00:18 +0200

    routes, controller, views, model

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,39 @@
 class BookingsController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :set_costume, only: [:new, :create]
 
   def new
     @booking = Booking.new
-    @costume = Costume.find(params[:costume_id])
   end
 
   def create
     @booking = Booking.new(booking_params)
-    @costume = Costume.find(params[:costume_id])
     @booking.costume = @costume
-    @booking.save
-    redirect_to restaurant_path(@costume)
+    @booking.user = current_user
+
+    Rails.logger.info "Booking Params: #{booking_params.inspect}"
+    Rails.logger.info "Booking Valid: #{@booking.valid?}"
+    Rails.logger.info "Booking Errors: #{@booking.errors.full_messages}"
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new, alert: "Booking failed. Please try again"
+    end
   end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
+  def set_costume
+    @costume = Costume.find(params[:costume_id])
+  end
+
 end
