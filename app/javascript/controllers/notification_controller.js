20db571364c7f981f83a411786927ab1f18f16d7
Robert Baufeld <rbaufeld@posteo.de>
2024-06-03 21:36:02 +0200

    notifications are now staying for 5 seconds in the top right corner

--- app/javascript/controllers/notification_controller.js
+++ app/javascript/controllers/notification_controller.js
@@ -3,8 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="notification"
 export default class extends Controller {
   connect() {
+    // This timeout removes the notification after 5 seconds
+    const notificationElement = this.element;
     setTimeout(() => {
-      this.element.remove();
+      notificationElement.remove();
     }, 5000);
   }
 }
